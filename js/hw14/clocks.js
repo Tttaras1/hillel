9ebdec2d8898ec38c64f8e07a2bafc4577bc9dcd
Tttaras1 <43076444+Tttaras1@users.noreply.github.com>
2018-11-01 23:15:20 +0200

    fixed table function

--- js/hw14/clocks.js
+++ js/hw14/clocks.js
@@ -1,10 +1,15 @@
 ﻿let time = {}
+let dateNow = {}
 
 function changeTime() {
     let date = new Date();
     let hours = document.querySelector('.hours');
     let minutes = document.querySelector('.minutes');
     let seconds = document.querySelector('.seconds');
+
+    let days = document.querySelector('.days');
+    let months = document.querySelector('.months');
+    let years = document.querySelector('.years');
 
     function setNum(num) {
         return (num < 10)
@@ -21,9 +26,25 @@ function changeTime() {
                     : ':'}`;
         }
     }
+
+    function getDate(htmlElement, newDate) {
+        if (dateNow[newDate] != date[newDate]()) {
+            dateNow[newDate] = date[newDate]();
+            htmlElement.innerHTML = setNum(dateNow[newDate]) +
+                `${(htmlElement == years)
+                    ? ''
+                    : '.'}`;
+        }
+    }
     getTime(hours, 'getHours');
     getTime(minutes, 'getMinutes');
     getTime(seconds, 'getSeconds');
+
+
+    getDate(days, 'getDay');
+    getDate(months, 'getMonth');
+    getDate(years, 'getFullYear');
     setTimeout(changeTime, 1000)
 }
 changeTime()
+
--- js/hw14/table.js
+++ js/hw14/table.js
@@ -3,23 +3,13 @@ function asd() {
     let tdata = document.getElementById("data");
     let table = document.getElementById("table");
     table.innerHTML = ''
-
-    let tr = document.createElement("tr");
-    table.appendChild(tr);
     for (let n = 0; n < tdata.value; n++) {
-        let td = document.createElement("td");
-        tr.appendChild(td);
-    }
-    for (let i = 1; i < trow.value; i++) {
-        let trClone = document.getElementsByTagName("tr")[0].cloneNode(true);
-        table.appendChild(trClone);
-    }
-
-    for (let i = 0; i < trow.value; i++) {
-        let trTitle = document.getElementsByTagName("tr")[i]
-        for (let n = 0; n < tdata.value; n++) {
-            trTitle.getElementsByTagName("td")[n].title = `${i+1} : ${n+1}`;
-            
+        let tr = document.createElement("tr");
+        table.appendChild(tr);
+        for (let i = 0; i < trow.value; i++) {
+            let td = document.createElement("td");
+            tr.appendChild(td);
+            tr.getElementsByTagName("td")[i].title = `${i + 1} : ${n + 1}`;
         }
     }
 }
