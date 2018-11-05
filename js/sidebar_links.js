cd5cb249b80909bb584c04aaa96c2318b031a9c7
Tttaras1 <43076444+Tttaras1@users.noreply.github.com>
2018-11-05 22:19:48 +0200

    added tested sidebar links

--- js/sidebar_links.js
+++ js/sidebar_links.js
@@ -1,4 +1,22 @@
 function asdsad() {
-    console.log(123)
+    let containers = document.getElementsByClassName('sidebar__item');
 
+    Array.prototype.forEach.call(containers, function(item) {
+        item.addEventListener("click", sidebarLinks);
+    }) 
+
+    function sidebarLinks() {
+        Array.prototype.forEach.call(containers, function(item) {
+            item.classList.remove('active')
+        }) 
+        console.log(this.textContent);
+        console.log(document.querySelector("#" + this.TextContent));
+
+
+
+        this.classList.add('active');
+    }
 }
+
+asdsad()
+
