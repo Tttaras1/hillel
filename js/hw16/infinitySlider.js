5b1dd27b114b2752d9f97130c8593785bc0c412f
Tttaras1 <43076444+Tttaras1@users.noreply.github.com>
2018-11-07 15:58:34 +0200

    slider is ok

--- js/hw16/infinitySlider.js
+++ js/hw16/infinitySlider.js
@@ -1,36 +1,13 @@
 function InfSlider() {
-    let container = document.getElementById('HW_16');
-    let slideNumber = 0;
-    let slider = document.createElement('div');
-    let sliderContainer = document.createElement('ul');
-    let sliderContainerList;
-    slider.classList.add('infinitySlider');
-    document.getElementById('infinitySlider').addEventListener('click', addSlider);
-    let imageSrc1 = '../../img/slider1.jpg';
-    let imageSrc2 = '../../img/slider2.jpg';
-    let imageSrc3 = '../../img/slider3.jpg';
-    let imgArr = [imageSrc1, imageSrc2, imageSrc3];
-    function addSlider() {
-        console.log('succes')
-        document.getElementById('HW_16').appendChild(slider);
-        slider.appendChild(sliderContainer);
-        sliderContainer.classList.add('infinitySlider__container');
-        imgArr.forEach(function (item, i) {
-            console.log(i + item);
-            sliderContainerList = document.createElement('li');
-            sliderContainerList.classList.add('infinitySlider__sliderContainer');
-            sliderContainerImg = document.createElement('img');
-            sliderContainerImg.setAttribute('src', item);
-            sliderContainerImg.classList.add('infinitySlider__sliderImage');
-            sliderContainer.appendChild(sliderContainerList);
-            sliderContainerList.appendChild(sliderContainerImg);
-        })
-
-        setTimeout(startSlider, 2000)
-    }
-
-    function startSlider() {
-
+    let sliderArr = ['slider1.jpg', 'slider2.jpg', 'slider3.jpg' ];
+    let container = document.querySelector('.infinitySlider');
+    let asd;
+    changePicture()
+    function changePicture() {
+        container.style.backgroundImage = "url(../../img/" + sliderArr[0] + ")";
+        asd = sliderArr.shift();
+        sliderArr[sliderArr.length] = asd;
+        setTimeout(changePicture, 2000)
     }
 }
 
