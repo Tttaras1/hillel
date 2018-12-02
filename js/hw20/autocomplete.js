668a27d02fa333513f1fb7af9117ecd42b4a381a
Tttaras1 <43076444+Tttaras1@users.noreply.github.com>
2018-12-02 18:34:59 +0200

    added weather function

--- js/hw20/autocomplete.js
+++ js/hw20/autocomplete.js
@@ -1,22 +1,37 @@
-function autocomplete() {
-    let ltd, lng;
+function weather() {
+    let textContainer = document.querySelector('.header__weather');
     let container = document.querySelector('[name="city"]');
     let autocomplete = new google.maps.places.Autocomplete(container);
-    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+    google.maps.event.addListener(autocomplete, 'place_changed', checkWeather);
+    if (localStorage.ltd && localStorage.lng) {
+        let ltd = localStorage.ltd;
+        let lng = localStorage.lng;
+        checkWeather(ltd, lng);
+    }
+    function checkWeather(ltd, lng) {
         let place = autocomplete.getPlace();
-        ltd = place.geometry.location.lat();
-        lng = place.geometry.location.lng();
-        let xhr = new XMLHttpsRequest();
-        xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=48.464717&lon=35.0461&appid=ec27c25d830d93288cf58c3b568da350', true);
-        xhr.send();
-        if (xhr.status != 200) {
-            console.log(xhr.responseText)
-            console.log(xhr.status)
-            console.log('error')
-        } else {
-            console.log(123);
+        ltd = ltd || place.geometry.location.lat();
+        lng = lng || place.geometry.location.lng();
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${ltd}&lon=${lng}&appid=ec27c25d830d93288cf58c3b568da350`, true);
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 400) {
+                let data = JSON.parse(xhr.responseText);
+                console.log(data);
+                let temperature = Math.round(data.main.temp - 273.15);
+                let weather = data.weather[0].description;
+                textContainer.innerHTML = ('Current temperature: ' + temperature + 'C;' + weather + '<br />' + data.name);
+            } else {
+                console.log('error:' + xhr.status);
+            }
         }
-    })
+        xhr.send()
+        if (localStorage.ltd != ltd && localStorage.lng != lng) {
+            localStorage.setItem('ltd', ltd);
+            localStorage.setItem('lng', lng)
+        }
+    }
+
 }
 
-google.maps.event.addDomListener(window, 'load', autocomplete);
+google.maps.event.addDomListener(window, 'load', weather);
